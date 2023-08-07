93df9d8b3921e118ac36e8d47e26602232305b03
Kristian Rosales <r.kristian40@gmail.com>
2023-08-07 15:21:07 -0500

    finishing up

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -33,26 +33,34 @@ function RenderLandingPage(props) {
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
       <div className="graphs-section">
-        <img
-          src={GrantRatesByOfficeImg}
-          alt="Office Graph"
-          className="graph-img"
-        />
-        <p className="graph-descriptions">Search Grant Rates By Office</p>
+        <div className="graph-container">
+          <img
+            src={GrantRatesByOfficeImg}
+            alt="Office Graph"
+            className="graph-img"
+          />
+          <p className="graph-descriptions">Search Grant Rates By Office</p>
+        </div>
 
-        <img
-          src={GrantRatesByNationalityImg}
-          alt="Nationality Graph"
-          className="graph-img"
-        />
-        <p className="graph-descriptions">Search Grant Rates By Nationality</p>
+        <div className="graph-container">
+          <img
+            src={GrantRatesByNationalityImg}
+            alt="Nationality Graph"
+            className="graph-img"
+          />
+          <p className="graph-descriptions">
+            Search Grant Rates By Nationality
+          </p>
+        </div>
 
-        <img
-          src={GrantRatesOverTimeImg}
-          alt="Over Time Graph"
-          className="graph-img"
-        />
-        <p className="graph-descriptions">Search Grant Rates Over Time</p>
+        <div className="graph-container">
+          <img
+            src={GrantRatesOverTimeImg}
+            alt="Over Time Graph"
+            className="graph-img"
+          />
+          <p className="graph-descriptions">Search Grant Rates Over Time</p>
+        </div>
       </div>
 
       <div className="view-more-data-btn-container">
@@ -83,9 +91,40 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+
+        {/* Idea before implementing was to have 3 divs with the same classname for easy styling. Inside of the divs were a h1 and a p tag that were easily  */}
         <div className="bottom-section">
-          <PageNav />
+          <h1>Systemic Disparity Insights</h1>
+
+          <div className="percentage-container">
+            <div className="percentages-row">
+              <h1>36%</h1>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscar year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+
+            <div className="percentages-row">
+              <h1>5%</h1>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+
+            <div className="percentages-row">
+              <h1>6x lower</h1>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
         </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
